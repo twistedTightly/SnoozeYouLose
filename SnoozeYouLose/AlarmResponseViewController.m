dd75c3c66a76721b5721b74adfc67d17d3866d8d
jcobian <jcobian@nd.edu>
2015-01-18 10:49:35 -0500

    connected snooze button. working on snooze confirmation

--- SnoozeYouLose/AlarmResponseViewController.m
+++ SnoozeYouLose/AlarmResponseViewController.m
@@ -78,6 +78,7 @@
 
     }
     [defaults synchronize];
+    
 }
 
 /*
---
+++ SnoozeYouLose/SnoozeConfirmationViewController.m
@@ -0,0 +1,37 @@
+//
+//  SnoozeConfirmationViewController.m
+//  SnoozeYouLose
+//
+//  Created by Jonathan Cobian on 1/18/15.
+//  Copyright (c) 2015 Maribeth, Jonathan, Halli, Rita. All rights reserved.
+//
+
+#import "SnoozeConfirmationViewController.h"
+
+@interface SnoozeConfirmationViewController ()
+
+@end
+
+@implementation SnoozeConfirmationViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
