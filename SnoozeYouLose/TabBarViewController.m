c0a33f147a28473c5cc48a0266101ba2db1d6fd9
twistedTightly <mrauh1@nd.edu>
2015-01-17 22:36:31 -0500

    Icons for tab view almost fixed (blue and grey)

--- SnoozeYouLose/TabBarViewController.m
+++ SnoozeYouLose/TabBarViewController.m
@@ -18,8 +18,6 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    //[[Venmo sharedInstance] logout];
-    // VENMO TESTING
     // Set Venmo payments to use the API by default, as opposed to switching to the app
     [[Venmo sharedInstance] setDefaultTransactionMethod:VENTransactionMethodAPI];
     
