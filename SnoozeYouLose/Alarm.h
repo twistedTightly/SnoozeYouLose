c048947efdd1194d305331a0445fb9dc8dd23581
Shuyang Li <shuyang.li.95@gmail.com>
2015-01-18 00:35:18 -0500

    Change repeat days into bit mask instead of mutable array

--- SnoozeYouLose/Alarm.h
+++ SnoozeYouLose/Alarm.h
@@ -8,12 +8,23 @@
 
 #import <Foundation/Foundation.h>
 
+typedef NS_OPTIONS(NSUInteger, RepeatDayOfWeekT) {
+    kRepeatDayOfWeekNotSet      = 0,
+    kRepeatDayOfWeekSunday      = 1 << 0,
+    kRepeatDayOfWeekMonday      = 1 << 1,
+    kRepeatDayOfWeekTuesday     = 1 << 2,
+    kRepeatDayOfWeekWednesday   = 1 << 3,
+    kRepeatDayOfWeekThursday    = 1 << 4,
+    kRepeatDayOfWeekFriday      = 1 << 5,
+    kRepeatDayOfWeekSaturday    = 1 << 6
+};
+
 @interface Alarm : NSObject <NSCoding>
 
 @property (strong, nonatomic) NSDate *alarmDate;
 @property (strong, nonatomic) NSString *friendName;
 @property (strong, nonatomic) NSNumber *snoozeCost;
 @property (nonatomic) BOOL isOn;
-@property (strong, nonatomic) NSMutableArray *repeatDays;
+@property (nonatomic) NSUInteger repeatDays;
 
 @end
--- SnoozeYouLose/RepeatDayOfWeekTableViewController.h
+++ SnoozeYouLose/RepeatDayOfWeekTableViewController.h
@@ -9,17 +9,6 @@
 #import <UIKit/UIKit.h>
 #import "Alarm.h"
 
-typedef NS_OPTIONS(NSUInteger, RepeatDayOfWeekT) {
-    kRepeatDayOfWeekNotSet      = 0,
-    kRepeatDayOfWeekSunday      = 1 << 0,
-    kRepeatDayOfWeekMonday      = 1 << 1,
-    kRepeatDayOfWeekTuesday     = 1 << 2,
-    kRepeatDayOfWeekWednesday   = 1 << 3,
-    kRepeatDayOfWeekThursday    = 1 << 4,
-    kRepeatDayOfWeekFriday      = 1 << 5,
-    kRepeatDayOfWeekSaturday    = 1 << 6
-};
-
 @interface RepeatDayOfWeekTableViewController : UITableViewController
 
 @property (strong, nonatomic) Alarm *alarm;
